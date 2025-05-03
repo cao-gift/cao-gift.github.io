219ad387a663267bf66633724c1561c2fa11bb37
SHIYUAN <65548967+cao-gift@users.noreply.github.com>
2025-05-03 21:47:00 +0800

    Create refresh-dogecloud.py

---
+++ .github/refresh-dogecloud.py
@@ -0,0 +1,58 @@
+from hashlib import sha1
+import hmac
+import requests
+import json
+import urllib
+import os
+
+def dogecloud_api(api_path, data={}, json_mode=False):
+    """
+    调用多吉云API
+
+    :param api_path:    调用的 API 接口地址，包含 URL 请求参数 QueryString，例如：/console/vfetch/add.json?url=xxx&a=1&b=2
+    :param data:        POST 的数据，字典，例如 {'a': 1, 'b': 2}，传递此参数表示不是 GET 请求而是 POST 请求
+    :param json_mode:   数据 data 是否以 JSON 格式请求，默认为 false 则使用表单形式（a=1&b=2）
+
+    :type api_path: string
+    :type data: dict
+    :type json_mode bool
+
+    :return dict: 返回的数据
+    """
+
+    # 这里替换为你的多吉云永久 AccessKey 和 SecretKey，可在用户中心 - 密钥管理中查看
+    # 请勿在客户端暴露 AccessKey 和 SecretKey，否则恶意用户将获得账号完全控制权
+    access_key = os.environ["ACCESS_KEY"]
+    secret_key = os.environ["SECRET_KEY"]
+
+    body = ''
+    mime = ''
+    if json_mode:
+        body = json.dumps(data)
+        mime = 'application/json'
+    else:
+        body = urllib.parse.urlencode(data) # Python 2 可以直接用 urllib.urlencode
+        mime = 'application/x-www-form-urlencoded'
+    sign_str = api_path + "\n" + body
+    signed_data = hmac.new(secret_key.encode('utf-8'), sign_str.encode('utf-8'), sha1)
+    sign = signed_data.digest().hex()
+    authorization = 'TOKEN ' + access_key + ':' + sign
+    response = requests.post('https://api.dogecloud.com' + api_path, data=body, headers = {
+        'Authorization': authorization,
+        'Content-Type': mime
+    })
+    return response.json()
+
+
+url_list = [
+    "https://cpen.top/",
+]
+
+api = dogecloud_api('/cdn/refresh/add.json', {
+    'rtype': 'path',
+    'urls': json.dumps(url_list)
+})
+if api['code'] == 200:
+    print(api['data']['task_id'])
+else:
+    print("api failed: " + api['msg']) # 失败
